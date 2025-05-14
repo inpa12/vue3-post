0257bfbc7594be474dbfe27fc0536c44e5e7661e
inpa12 <inpa12@gmail.com>
2025-05-14 18:29:25 +0900

    20250514-02

--- src/components/posts/PostItem.vue
+++ src/components/posts/PostItem.vue
@@ -1,17 +1,24 @@
 <template>
-    <div class="card">
-        <div class="card-body">
-            <h5 class="card-title">{{ title }}</h5>
-            <p class="card-text">
-                {{ content }}
-            </p>
-            <p class="text-muted">{{ createdAt }}</p>
-        </div>
-    </div>
+    <AppCard>
+        <h5 class="card-title">{{ title }}</h5>
+        <p class="card-text">
+            {{ content }}
+        </p>
+        <p class="text-muted">{{ createdAt }}</p>
+        <template #footer>
+            <div class="d-flex flex-row-reverse">
+                <button class="btn p-0" @click.stop="$emit('modal')">
+                    <i class="bi bi-emoji-sunglasses"></i>
+                </button>
+            </div>
+        </template>
+    </AppCard>
 </template>
 
 <script setup>
-const props = defineProps({
+import AppCard from '@/components/AppCard.vue'
+
+defineProps({
     title: {
         type: String,
         required: true,
@@ -23,6 +30,7 @@ const props = defineProps({
         type: [String, Date, Number],
     },
 })
+defineEmits(['modal'])
 </script>
 
 <style lang="scss" scoped></style>
--- src/views/posts/PostEditView.vue
+++ src/views/posts/PostEditView.vue
@@ -10,7 +10,8 @@
                 <button class="btn btn-primary">수정</button>
             </template>
         </PostForm>
-        <AppAlert :show="showAlert" :message="alertMessage" :type="alertType" />
+        <!-- <AppAlert :show="showAlert" :message="alertMessage" :type="alertType" /> -->
+        <AppAlert :items="alerts" />
     </div>
 </template>
 
@@ -35,7 +36,7 @@ const fetchPosts = async () => {
         setForm(data)
     } catch (error) {
         console.error(error)
-        vAlert('네트워크 오류!')
+        vAlert(error.message)
     }
 }
 const setForm = ({ title, content }) => {
@@ -47,25 +48,21 @@ fetchPosts()
 const edit = async () => {
     try {
         await updatePost(id, { ...form.value })
-        //router.push({ name: 'PostDetail', params: { id } })
         vAlert('수정이 완료 되었습니다.', 'success')
     } catch (error) {
         console.error(error)
+        vAlert(error.message)
     }
 }
 
 const goDetailPage = () => router.push({ name: 'PostDetail', params: { id } })
 
 // alert
-const showAlert = ref(false)
-const alertMessage = ref('')
-const alertType = ref('')
+const alerts = ref([])
 const vAlert = (message, type = 'error') => {
-    showAlert.value = true
-    alertMessage.value = message
-    alertType.value = type
+    alerts.value.push({ message, type })
     setTimeout(() => {
-        showAlert.value = false
+        alerts.value.shift()
     }, 2000)
 }
 </script>
